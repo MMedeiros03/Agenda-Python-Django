cef78475824a87cec4ca35f5d19719c3c5755a87
MMedeiros03 <matheusmedeiros2003@gmail.com>
2021-11-04 10:43:35 -0300

    Adicionando pasta principal do projeto

---
+++ agenda/core/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import base
+from django.shortcuts import render
+from datetime import datetime, timedelta
+# Create your models here.
+class Evento(models.Model):
+    titulo       = models.CharField(max_length=50)
+    descricao    = models.TextField(max_length=200,null=True,blank=True)
+    data_evento  = models.DateTimeField()
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario      = models.ForeignKey(User,on_delete=models.CASCADE,related_name="usuario")
+    local        = models.CharField(max_length=200,null=True,blank=True)
+
+    class Meta:
+        db_table = "Evento"
+
+    def __str__(self):
+        return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M horas')
+    
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
+
+    def get_evento_atrasado(self):
+        if self.data_evento < datetime.now():
+            return True
+        else:
+            return False
